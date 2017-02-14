ae5fe66cd7eec00c51ff164e6b53558185f07e63
Mariano Cabezas <mcabezas@eia.udg.edu>
2017-02-14 16:51:01 +0100

    - Updated division by zero cases. - Added a main function to evaluate folders (all folders must contain a init_names.py file with a get_names from_folder that loads the gt and estimated nii file names).

--- metrics.py
+++ metrics.py
@@ -1,3 +1,7 @@
+import os
+import sys
+import argparse
+from nibabel import load as load_nii
 import numpy as np
 from skimage.measure import label as bwlabeln
 from scipy.ndimage.morphology import binary_erosion as imerode
@@ -82,12 +86,12 @@ def fp_fraction_det(target, estimated):
 
 def dsc_seg(target, estimated):
     a_plus_b = np.sum(np.sum(as_logical(target)) + np.sum(as_logical(estimated)))
-    return 2.0 * true_positive_seg(target, estimated) / a_plus_b
+    return 2.0 * true_positive_seg(target, estimated) / a_plus_b if a_plus_b > 0 else 0.0
 
 
 def dsc_det(target, estimated):
     a_plus_b = (np.max(bwlabeln(as_logical(target))) + np.max(bwlabeln(as_logical(estimated))))
-    return 2.0 * true_positive_det(target, estimated) / a_plus_b
+    return 2.0 * true_positive_det(target, estimated) / a_plus_b if a_plus_b > 0 else 0.0
 
 
 def surface_distance(target, estimated, spacing=[1, 1, 3]):
@@ -110,3 +114,57 @@ def average_surface_distance(target, estimated, spacing):
 def hausdorff_distance(target, estimated, spacing):
     distances = surface_distance(target, estimated, spacing)
     return np.max([np.max(distances[0]), np.max(distances[1])])
+
+
+def main():
+    # Parse command line options
+    parser = argparse.ArgumentParser(description='Test different nets with 3D data.')
+    group = parser.add_mutually_exclusive_group()
+
+    folder_help = 'Folder with the files to evaluate. Remember to include a init_names.py for the evaluation pairs.'
+    files_help = 'Pair of files to be compared. The first is the GT and the second the file you wnat to evaluate.'
+
+    group.add_argument('-f', '--folder', default='/home/mariano/DATA/LST/', help=folder_help)
+    group.add_argument('--files', default=['gt_mask.nii', 'auto_mask.nii'], nargs=2, help=files_help)
+    args = parser.parse_args()
+
+    if args.folder:
+        folder_name = args.folder
+        sys.path = sys.path + [folder_name]
+        from init_names import get_names_from_folder
+        gt_names, all_names = get_names_from_folder(folder_name)
+
+    elif args.files:
+        folder_name = os.getcwd()
+        gt_names = [args.files[0]]
+        all_names = [[args.files[1]]]
+
+    with open(os.path.join(folder_name, 'results.csv'), 'w') as f:
+        for gt_name, names in zip(gt_names, all_names):
+            print('\033[32mEvaluating with ground truth \033[32;1m' + gt_name + '\033[0m')
+
+            gt_nii = load_nii(gt_name)
+            gt = gt_nii.get_data()
+            spacing = dict(gt_nii.header.items())['pixdim'][1:4]
+
+            for name in names:
+                name = ''.join(name)
+                print('\033[32m-- vs \033[32;1m' + name + '\033[0m')
+                lesion = load_nii(name).get_data()
+                dist = average_surface_distance(gt, lesion, spacing)
+                tpfv = tp_fraction_seg(gt, lesion)
+                fpfv = fp_fraction_seg(gt, lesion)
+                dscv = dsc_seg(gt, lesion)
+                tpfl = tp_fraction_det(gt, lesion)
+                fpfl = fp_fraction_det(gt, lesion)
+                dscl = dsc_det(gt, lesion)
+                tp = true_positive_det(lesion, gt)
+                gt_d = num_regions(gt)
+                lesion_s = num_voxels(lesion)
+                gt_s = num_voxels(gt)
+                measures = (gt_name, name, dist, tpfv, fpfv, dscv, tpfl, fpfl, dscl, tp, gt_d, lesion_s, gt_s)
+                f.write('%s;%s;%f;%f;%f;%f;%f;%f;%f;%d;%d;%d;%d\n' % measures)
+
+
+if __name__ == '__main__':
+    main()
