5830eccd6fce083b23224fdccd56a27415e96b67
Mariano Cabezas <mcabezas@eia.udg.edu>
2017-06-07 12:26:12 +0200

    - Added requirements - Added analysis by region size to the metrics file

--- metrics.py
+++ metrics.py
@@ -20,7 +20,7 @@ def masks_by_size(mask, sizes):
     blobs, labels, areas = regionprops(mask)
     labels_list = [[l for l, a in zip(labels, areas) if a >= mins & a < maxs]
                    for mins, maxs in zip(sizes[:-1], sizes[1:])]
-    labels_list.append([l for l, a in zip(labels, areas) if a > sizes[-1]])
+    labels_list.append([l for l, a in zip(labels, areas) if a >= sizes[-1]])
     submasks = [reduce(lambda x, y: np.logical_or(x, y), [np.equal(blobs, l) for l in nu_labels if nu_labels])
                 if nu_labels else np.zeros_like(mask) for nu_labels in labels_list]
     return submasks
