28bcf45b794995dedfa4e47e8e577544ab4a9d24
mariano <mcabezas@eia.udg.edu>
2020-03-10 15:11:09 +1100

    - added some comments (will work on more later)

--- models.py
+++ models.py
@@ -9,7 +9,23 @@ from .utils import time_to_string
 
 
 class BaseModel(nn.Module):
+    """"
+    This is the baseline model to be used for any of my networks. The idea
+    of this model is to create a basic framework that works similarly to
+    keras, but flexible enough.
+    For that reason, I have "embedded" the typical pytorch main loop into a
+    fit function and I have defined some intermediate functions and callbacks
+    to alter the main loop. By itself, this model can train any "normal"
+    network with different losses and scores for training and validation.
+    It can be easily extended to create adversarial networks (which I have done
+    in other repositories) and probably to other more complex problems.
+    The network also includes some print functions to check the current status.
+    """
     def __init__(self):
+        """
+        Main init. By default some parameters are set, but they should be
+        redefined on networks inheriting that model.
+        """
         super().__init__()
         # Init values
         self.init = True
@@ -32,20 +48,44 @@ class BaseModel(nn.Module):
         self.acc = None
 
     def forward(self, *inputs):
+        """
+
+        :param inputs: Inputs to the forward function. We are passing the
+         contents by reference, so if there are more than one input, they
+         will be separated.
+        :return: Nothing. This has to be reimplemented for any class.
+        """
         return None
 
     def mini_batch_loop(
             self, data, train=True
     ):
+        """
+        This is the main loop. It's "generic" enough to account for multiple
+        types of data (target and input) and it differentiates between
+        training and testing. While inherently all networks have a training
+        state to check, here the difference is applied to the kind of data
+        being used (is it the validation data or the training data?). Why am
+        I doing this? Because there might be different metrics for each type
+        of data. There is also the fact that for training, I really don't care
+        about the values of the losses, since I only want to see how the global
+        value updates, while I want both (the losses and the global one) for
+        validation.
+        :param data: Dataloader for the network.
+        :param train: Whether to use the training dataloader or the validation
+         one.
+        :return:
+        """
         losses = list()
         mid_losses = list()
         accs = list()
         n_batches = len(data)
         for batch_i, (x, y) in enumerate(data):
-            # We train the model and check the loss
+            # In case we are training the the gradient to zero.
             if self.training:
                 self.optimizer_alg.zero_grad()
 
+            # First, we do a forward pass through the network.
             torch.cuda.synchronize()
             if isinstance(x, list):
                 x_cuda = tuple(x_i.to(self.device) for x_i in x)
@@ -53,8 +93,9 @@ class BaseModel(nn.Module):
             else:
                 pred_labels = self(x.to(self.device))
 
-            # Training losses
+            # After that, we can compute the relevant losses.
             if train:
+                # Training losses (applied to the training data)
                 batch_losses = [
                     l_f['weight'] * l_f['f'](pred_labels, y)
                     for l_f in self.train_functions
@@ -64,8 +105,8 @@ class BaseModel(nn.Module):
                     batch_loss.backward()
                     self.optimizer_alg.step()
 
-            # Validation losses
             else:
+                # Validation losses (applied to the validation data)
                 batch_losses = [
                     l_f['f'](pred_labels, y)
                     for l_f in self.val_functions
@@ -84,6 +125,7 @@ class BaseModel(nn.Module):
             torch.cuda.synchronize()
             torch.cuda.empty_cache()
 
+            # It's important to compute the global loss in both cases.
             loss_value = batch_loss.tolist()
             losses.append(loss_value)
 
@@ -95,10 +137,14 @@ class BaseModel(nn.Module):
                 batch_i, n_batches, loss_value, np.mean(losses)
             )
 
+        # Mean loss of the global loss (we don't need the loss for each batch).
         mean_loss = np.mean(losses)
+
         if train:
             return mean_loss
         else:
+            # If using the validation data, we actually need to compute the
+            # mean of each different loss.
             mean_losses = np.mean(list(zip(*mid_losses)), axis=1)
             np_accs = np.array(list(zip(*accs)))
             mean_accs = np.mean(np.accs, axis=1) if np_accs.size > 0 else []
@@ -121,20 +167,41 @@ class BaseModel(nn.Module):
         acc_names = [
             '{:^6s}'.format(a_f['name']) for a_f in self.acc_functions
         ]
+        l_bars = '--|--'.join(
+            ['-' * 5] * 2 +
+            ['-' * 6] * (len(l_names[2:]) + len(acc_names)) +
+            ['-' * 3]
+        )
+        l_hdr = '  |  '.join(l_names + acc_names + ['drp'])
+        # Since we haven't trained the network yet, we'll assume that the
+        # initial values are the best ones.
         self.best_state = deepcopy(self.state_dict())
         self.best_opt = deepcopy(self.optimizer_alg.state_dict())
         t_start = time.time()
 
         # Initial losses
+        # This might seem like an unnecessary step (and it actually often is)
+        # since it wastes some time checking the output with the initial
+        # weights. However, it's good to check that the network doesn't get
+        # worse than a random one (which can happen sometimes).
         if self.init:
+            # We are looking for the output, without training, so no need to
+            # use grad.
             with torch.no_grad():
                 self.t_val = time.time()
+                # We set the network to eval, for the same reason.
                 self.eval()
+                # Training losses.
                 best_loss_tr = self.mini_batch_loop(train_loader)
+                # Validation losses.
                 best_loss_val, best_losses, best_acc = self.mini_batch_loop(
                     val_loader, False
                 )
+                # Doing this also helps setting an initial best loss for all
+                # the necessary losses.
                 if verbose:
+                    # This is just the print for each epoch, but including the
+                    # header.
                     # Mid losses check
                     epoch_s = '\033[32mInit     \033[0m'
                     tr_loss_s = '\033[32m{:7.4f}\033[0m'.format(best_loss_tr)
@@ -151,12 +218,6 @@ class BaseModel(nn.Module):
 
                     drop_s = '{:5.3f}'.format(self.dropout)
 
-                    l_bars = '--|--'.join(
-                        ['-' * 5] * 2 +
-                        ['-' * 6] * (len(l_names[2:]) + len(acc_names)) +
-                        ['-' * 3]
-                    )
-                    l_hdr = '  |  '.join(l_names + acc_names + ['drp'])
                     print('\033[K', end='')
                     whites = ' '.join([''] * 12)
                     print('{:}Epoch num |  {:}  |'.format(whites, l_hdr))
@@ -167,6 +228,12 @@ class BaseModel(nn.Module):
                     )
                     print(final_s)
         else:
+            # If we don't initialise the losses, we'll just take the maximum
+            # ones (inf, -inf) and print just the header.
+            print('\033[K', end='')
+            whites = ' '.join([''] * 12)
+            print('{:}Epoch num |  {:}  |'.format(whites, l_hdr))
+            print('{:}----------|--{:}--|'.format(whites, l_bars))
             best_loss_tr = np.inf
             best_loss_val = np.inf
             best_losses = [np.inf] * len(self.val_functions)
@@ -176,6 +243,7 @@ class BaseModel(nn.Module):
             # Main epoch loop
             self.t_train = time.time()
             self.train()
+            # First we train and check if there has been an improvement.
             loss_tr = self.mini_batch_loop(train_loader)
             improvement_tr = best_loss_tr > loss_tr
             if improvement_tr:
@@ -184,6 +252,7 @@ class BaseModel(nn.Module):
             else:
                 tr_loss_s = '{:7.4f}'.format(loss_tr)
 
+            # Then we validate and check all the losses
             with torch.no_grad():
                 self.t_val = time.time()
                 self.eval()
@@ -217,6 +286,8 @@ class BaseModel(nn.Module):
             ]
 
             # Patience check
+            # We check the patience to stop early if the network is not
+            # improving. Otherwise we are wasting resources and time.
             improvement_val = best_loss_val > loss_val
             loss_s = '{:7.4f}'.format(loss_val)
             if improvement_val:
@@ -264,15 +335,39 @@ class BaseModel(nn.Module):
             )
 
     def epoch_update(self, epochs):
+        """
+        Callback function to update something on the model after the epoch
+        is finished. To be reimplemented if necessary.
+        :param epochs: Maximum number of epochs
+        :return: Nothing.
+        """
         return None
 
     def dropout_update(self):
+        """
+        Callback function to update the dropout. To be reimplemented
+        if necessary. However, the main method already has some basic
+        scheduling
+        :param epochs: Maximum number of epochs
+        :return: Nothing.
+        """
         if self.final_dropout <= self.dropout:
             self.dropout = max(
                 self.final_dropout, self.dropout - self.ann_rate
             )
 
     def print_progress(self, batch_i, n_batches, b_loss, mean_loss):
+        """
+        Function to print the progress of a batch. It takes into account
+        whether we are training or validating and uses different colors to
+        show that. It's based on Keras arrow progress bar, but it only shows
+        the current (and current mean) training loss, elapsed time and ETA.
+        :param batch_i: Current batch number.
+        :param n_batches: Total number of batches.
+        :param b_loss: Current loss.
+        :param mean_loss: Current mean loss.
+        :return: None.
+        """
         init_c = '\033[0m' if self.training else '\033[38;5;238m'
         whites = ' '.join([''] * 12)
         percent = 20 * (batch_i + 1) // n_batches
