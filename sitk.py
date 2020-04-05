d984943ce2a4917a233616f5d1140cbab8521c65
marianocabezas <mcabezas@silver.udg.edu>
2020-04-05 12:15:18 +1000

    - changed verbosity for affine

--- sitk.py
+++ sitk.py
@@ -308,7 +308,8 @@ def itkrigid(
 
     # Setup for the multi-resolution framework.
     smoothing_sigmas = range(levels - 1, -1, -1)
-    print('\tSigmas {:}'.format(smoothing_sigmas))
+    if verbose > 1
+        print('\tSigmas {:}'.format(smoothing_sigmas))
     shrink_factor = [2**i for i in smoothing_sigmas]
     registration.SetShrinkFactorsPerLevel(shrinkFactors=shrink_factor)
     registration.SetSmoothingSigmasPerLevel(smoothingSigmas=smoothing_sigmas)
@@ -410,7 +411,8 @@ def itkaffine(
 
     # Setup for the multi-resolution framework.
     smoothing_sigmas = range(levels - 1, -1, -1)
-    print('\tSigmas {:}'.format(smoothing_sigmas))
+    if verbose > 1:
+        print('\tSigmas {:}'.format(smoothing_sigmas))
     shrink_factor = [2**i for i in smoothing_sigmas]
     registration.SetShrinkFactorsPerLevel(shrinkFactors=shrink_factor)
     registration.SetSmoothingSigmasPerLevel(smoothingSigmas=smoothing_sigmas)
