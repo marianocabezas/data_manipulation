9d7cb2048f823a583b8c5715e17654893250d490
marianocabezas <mcabezas@silver.udg.edu>
2020-04-07 12:38:37 +1000

    - fixed spatial transform when interpolation values are learned

--- layers.py
+++ layers.py
@@ -226,8 +226,9 @@ class SpatialTransformer(nn.Module):
                 )
             else:
                 values = tuple(map(get_point_value, cube_pts))
+                values = torch.stack(values, dim=0)
 
-            interp_vol = torch.sum(torch.stack(values, dim=0), dim=0)
+            interp_vol = torch.sum(values, dim=0)
 
         elif self.interp_method == 'nearest':
             # clip values
