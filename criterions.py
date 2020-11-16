5d08fbb3beba41fbaf350a52805f67c227b04dda
mariano <mcabezas@eia.udg.edu>
2020-11-16 14:23:46 +1100

    - changed patch size

--- criterions.py
+++ criterions.py
@@ -91,8 +91,8 @@ def dsc_loss(pred, target, smooth=1e-5):
     # There is only a class here.
     # DSC = 2 * | pred *union* target | / (| pred | + | target |)
     reduce_dims = tuple(range(1, len(dims)))
-    num = (2 * torch.sum(pred * target, dim=reduce_dims))
-    den = torch.sum(pred + target, dim=reduce_dims)
+    num = (2 * torch.sum(pred * target, dim=reduce_dims)) + smooth
+    den = torch.sum(pred + target, dim=reduce_dims) + smooth
     dsc_k = num / den
     dsc = 1 - torch.mean(dsc_k)
 
